7e1e0aa470673c8531398c147e3e9d41d7aebb26
王凯 <wangkai05@bilibili.com>
2022-10-08 22:28:08 +0800

    modify variable name

--- atexit.go
+++ atexit.go
@@ -8,18 +8,15 @@ import (
 )
 
 func Exec(f func() error) {
-	var sChan = make(chan os.Signal)
-	signal.Notify(sChan, syscall.SIGINT, syscall.SIGTERM)
-
+	var c = make(chan os.Signal)
+	signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
 	go func() {
-		for s := range sChan {
-			fmt.Printf("PID %d receives signal: %s\n", os.Getpid(), s)
-
+		for sig := range c {
+			fmt.Printf("PID %d receives signal: %s\n", os.Getpid(), sig)
 			if err := f(); err != nil {
 				println(err.Error())
 				os.Exit(1)
 			}
-
 			os.Exit(0)
 		}
 	}()
